934658e6f3d8820393e6d771b7da9731c73572eb
bcoe <bencoe@gmail.com>
2012-02-03 00:54:26 -0800

    Added bin directory moved the CLI stuff into there.

---
+++ bin/tcplock.js
@@ -0,0 +1,10 @@
+var TCPLock = require('tcplock').TCPLock,
+  args = require('argsparser').parse(),
+  puts = require('sys').puts;
+
+if (!args['-l'] || !args['-p']) {
+  puts('Options:\n\t-l listen port\n\t-p proxy port\n\t-h proxy host\n\t-n number of connections allowed')
+} else {
+  var lock = new TCPLock(args['-l'], args['-p'], args['-h'], args['-n']);
+  lock.start();
+}

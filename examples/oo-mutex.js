0a38093d4a5d503b90531181ec2ec157e84c517e
bcoe <bencoe@gmail.com>
2012-02-11 16:24:55 -0800

    Trying to get things to work in a bullet-proof fasion for the openoffice semaphore.

---
+++ examples/oo-mutex.js
@@ -0,0 +1,34 @@
+// Places a mutex in front of the Open Office Python-UNO bridge.
+var TCPLock = require('tcplock').TCPLock,
+	puts = require('util').puts,
+	exec = require('child_process').exec,
+	portscanner = require('portscanner');
+
+function waitForOOState(state, callback) {
+		(function checkForOOP() {
+			portscanner.checkPortStatus(8100, 'localhost', function(err, status) {
+				if (status == state) {
+					callback();
+				} else {
+					setTimeout(checkForOOP, 250);
+				}
+			});
+		})();	
+}
+
+var lock = new TCPLock({
+	listenPort: 9000,
+	proxyPort: 8100,
+	timeout: 20000 // Only alow OOP to run for 20 seconds.
+}, function(activateNextConnection) {
+	exec('killall -9 soffice.bin libreoffice soffice', function(error, stdout, stderr) {
+		
+		waitForOOState('closed', function() {
+			exec('libreoffice -headless -nofirststartwizard -accept="socket,host=0.0.0.0,port=8100;urp;StarOffice.Service"'); 
+			waitForOOState('open', function() {
+				activateNextConnection();
+			});
+		});
+		
+	});
+});
